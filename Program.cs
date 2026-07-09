4622838cd943bcf214200dafd02eaccd6bd4aea0
Elias <277282575+only9elias@users.noreply.github.com>
2026-07-09 12:40:06 +0200

    Edit: Added HelpService call inside Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,30 @@
 ﻿Console.WriteLine("Willkommen bei DevQuiz!");
 Console.WriteLine();
-var quizEngine = new QuizEngine();
-quizEngine.Start();
+bool running = true;
+while(running == true)
+{
+    Console.WriteLine("1: Quiz Starten\n2: HelpService aufrufen\n0: Beenden\n>>> ");
+    string? input = Console.ReadLine();
+
+    if (input == "1")
+    {
+        var quizEngine = new QuizEngine();
+        quizEngine.Start(); 
+    }
+    else if(input == "2")
+    {
+        HelpService.DisplayGeneralHelp();
+        HelpService.DisplayStartMenuHelp();
+        running = false;
+    }
+    else if(input == "0")
+    {
+        running = false;
+    }
+    else{
+        Console.WriteLine("\n\n\n");
+        };
+
+}
+
+Console.WriteLine("See you next time...");
