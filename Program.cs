03627ecd8405aa32d0d271ae10b43eefc12e6804
Noahqwq <you@example.com>
2026-07-09 12:19:14 +0200

    Added Startmenu

--- Program.cs
+++ Program.cs
@@ -1,4 +1,27 @@
 ﻿Console.WriteLine("Willkommen bei DevQuiz!");
 Console.WriteLine();
-var quizEngine = new QuizEngine();
-quizEngine.Start();
+bool running = true;
+while(running == true)
+{
+    Console.Clear();
+    Console.WriteLine("1: Quiz Starten\n2: HelpService aufrufen\n0: Beenden\n>>> ");
+    string? input = Console.ReadLine();
+
+    if (input == "1")
+    {
+        var quizEngine = new QuizEngine();
+        quizEngine.Start(); 
+    }
+    else if(input == "2")
+    {
+        Console.WriteLine("HelpService still in progress...");
+        running = false;
+    }
+    else if(input == "0")
+    {
+        running = false;
+    }
+    else{Console.WriteLine("\n\n\n")};
+
+}
+Console.WriteLine("See you next time...");
